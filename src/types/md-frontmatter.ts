c159b577a5e7b7ca536c671620c49955f2081eac
matth <matthewiva13@gmail.com>
2024-11-09 20:33:11 +0200

    frontmatter type

---
+++ src/types/md-frontmatter.ts
@@ -0,0 +1,29 @@
+type BaseFrontmatter = {
+  layout: string;
+  title: string;
+  description: string;
+  tags: string[];
+};
+
+type ProjectFrontmatter = {
+  [key: string]: unknown;
+} & BaseFrontmatter;
+
+type WorkFrontmatter = {
+  company: string;
+  domain: string;
+  position: string;
+
+  startDate: string;
+  endDate: string | null;
+} & BaseFrontmatter;
+
+type PostType = "project" | "work";
+
+type PostFrontmatter<T extends PostType> = T extends "work"
+  ? WorkFrontmatter
+  : T extends "project"
+    ? ProjectFrontmatter
+    : never;
+
+export type { PostType, PostFrontmatter, ProjectFrontmatter, WorkFrontmatter };
