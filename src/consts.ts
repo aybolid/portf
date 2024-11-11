d8aa0b54acff71dc1ccfe7b084cdb0117c5dd047
matth <matthewiva13@gmail.com>
2024-11-11 22:35:49 +0200

    comments raw

--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,9 @@ export const LINKEDIN_URL =
   "https://www.linkedin.com/in/matviy-ivanov-988940248/";
 
 export const EMAIL = "matviy13@proton.me";
+
+export const LINKS_ORDER = {
+  "/work-history": 1,
+  "/my-projects": 2,
+  "/say-smth": 3,
+};
