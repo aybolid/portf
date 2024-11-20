ec3025cffdd48012201f77b6942f72f6e35ff89f
matth <matthewiva13@gmail.com>
2024-11-20 12:09:57 +0200

    djinni link

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,6 @@ export const NAME = "Matviy Ivanov";
 export const GITHUB_URL = "https://github.com/aybolid";
 export const LINKEDIN_URL =
   "https://www.linkedin.com/in/matviy-ivanov-988940248/";
+export const DJINNI_URL = "https://djinni.co/q/cb0ea56cce/";
 
 export const EMAIL = "matviy13@proton.me";
